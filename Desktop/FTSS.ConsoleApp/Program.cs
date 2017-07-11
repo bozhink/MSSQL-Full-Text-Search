d7f6c59aee8489ed0064bd26e8cf28fcebea1098
Bozhin Karaivanov <bozhin.karaivanov@live.com>
2017-07-11 14:52:34 +0300

    Program update

--- Desktop/FTSS.ConsoleApp/Program.cs
+++ Desktop/FTSS.ConsoleApp/Program.cs
@@ -1,9 +1,36 @@
 ﻿namespace FTSS.ConsoleApp
 {
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using FTSS.Data;
+    using FTSS.Data.Interceptors;
+    using FTSS.Data.Models;
+
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var data = DoSearch("content").Result;
+
+            foreach (var item in data)
+            {
+                Console.WriteLine(item.NoteText);
+            }
+        }
+
+        public static async Task<Note[]> DoSearch(string text)
+        {
+            var s = FtsInterceptor.Fts(text);
+
+            using (var db = new FtssContext("Connection string"))
+            {
+                var query = db.Notes.Where(n => n.NoteText.Contains(s));
+
+                var data = await query.Take(10).ToArrayAsync();
+                return data;
+            }
         }
     }
 }
